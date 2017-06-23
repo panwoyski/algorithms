3c16dc9b265aa8ed09c57c2e6dea5e96c1c1acdc
woyski <jonak.t.o@gmail.com>
2017-06-23 21:43:22 +0200

    Ostream graph representation and solver prototype

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <map>
 #include <vector>
 #include <iterator>
+#include <unistd.h>
 
 std::string generateTestStream(); 
 
@@ -15,6 +16,10 @@ public:
         this->failsafe_add(a, b);
         this->failsafe_add(b, a);
     }
+
+    bool areVerticesConnected(unsigned, unsigned) const;
+
+    friend std::ostream & operator<<(std::ostream &, const Graph&);
 private:
     void failsafe_add(unsigned a, unsigned b) {
         if (m_adj.count(a) < 1)
@@ -23,6 +28,46 @@ private:
             m_adj[a].insert(b);
     }
 };
+
+bool Graph::areVerticesConnected(unsigned vertexA, unsigned vertexB) const {
+    auto visited = std::vector<bool>(false, m_adj.size());
+
+    return false;
+}
+
+std::ostream & operator<<(std::ostream& os, const Graph & graph) {
+    for (const auto & pair : graph.m_adj) {
+        os << pair.first << ':';
+        for (const auto & vertex : pair.second) {
+            os << vertex << " ";
+        }
+        os << std::endl;
+    }
+        
+    return os;
+}
+
+void parseInput(std::vector<std::string> tokens) {
+    auto graph = Graph();
+    bool sig1_present = false;
+    bool sig2_present = false;
+    int significant1 = std::stoi(tokens[0]);
+    int significant2 = std::stoi(tokens[1]);
+
+    std::cout << significant1 << std::endl;
+    std::cout << significant2 << std::endl;
+
+    for (std::size_t i = 2; i < tokens.size(); i += 2) {
+        int vertex_a = std::stoi(tokens[i]);
+        int vertex_b = std::stoi(tokens[i+1]);
+        sig1_present |= significant1 == vertex_a || significant1 == vertex_b;
+        sig2_present |= significant2 == vertex_a || significant2 == vertex_b;
+        graph.addEdge(vertex_a, vertex_b); 
+    }
+    std::cout << "Significant1 is " << (sig1_present ? "present" : "absent") << std::endl;
+    std::cout << "Significant2 is " << (sig2_present ? "present" : "absent") << std::endl;
+    std::cout << graph;
+}
 
 int main(int argc, const char * argv[]) {
     (void)argc;
@@ -33,9 +78,12 @@ int main(int argc, const char * argv[]) {
     std::vector<std::string> tokens{std::istream_iterator<std::string>{stream},
                                     std::istream_iterator<std::string>()};
 
-    for (const auto& token : tokens) {
-        std::cout << token << std::endl;
-    }
+    parseInput(tokens);
+    
+
+    //for (const auto& token : tokens) {
+    //    std::cout << token << std::endl;
+    //}
     return 0;
 }
 
